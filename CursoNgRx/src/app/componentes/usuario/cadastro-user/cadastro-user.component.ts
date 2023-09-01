a212c8de73f9e8694931d74c671e815c4430df82
Anderson Martins <BlackCode77.1980@gmail.com>
2023-09-01 07:43:30 -0300

    Modificando componente de cadastro e implementando o NgRx

--- CursoNgRx/src/app/componentes/usuario/cadastro-user/cadastro-user.component.ts
+++ CursoNgRx/src/app/componentes/usuario/cadastro-user/cadastro-user.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/Store/app.state';
+import * as fromUsuariosAction from 'src/app/Store/usuarios/usuarios.actions';
 import { UsuarioModel } from 'src/app/models/usuario-model';
-import { UsuarioServiceService } from 'src/app/repository/usuario-service.service';
 
 @Component({
   selector: 'app-cadastro-user',
@@ -8,44 +10,54 @@ import { UsuarioServiceService } from 'src/app/repository/usuario-service.servic
   styleUrls: ['./cadastro-user.component.scss'],
 })
 export class CadastroUserComponent {
-
   public model: UsuarioModel = { id: 0, nome: '', idade: 0, perfil: '' };
 
-  constructor(private userService: UsuarioServiceService) {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {}
 
-  //Salvar usuario no banco de dados e redirecionar para a ListagemUserComponent (Listagem de usuarios)
-  // e limpar o formulario de cadastro de usuario (this.model = {})
   addUsuario() {
-    if( this.model.id == 0 ){
-      // Se o id do usuário for igual a zero então salva no banco de dados
-      this.userService.addUsuario(this.model).subscribe(
-        (data) => {
-          this.model = data;
-
-          // limpando o formulario de cadastro de usuario
-          this.model = { id: 0, nome: '', idade: 0, perfil: '' };
-
-          console.log(data);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-
+    
+    if(this.model.id == 0){
+      console.log("Cadastro %%% ",this.model);
+      this.store.dispatch(
+      fromUsuariosAction.CreateUsuario({ payload: this.model })
+    );
     }else{
-      // Se o usuário já existir no banco de dados então atualiza o usuário
-      this.userService.updateUsuario(this.model).subscribe(
-        (data) => {
-          this.model = data;
-          console.log(data);
-        }
+      console.log('Atualiza %%% ', this.model);
+      this.store.dispatch(
+        fromUsuariosAction.UpdateUsuario({ payload: this.model })
       );
     }
-    console.log(this.model);
-    //this.model = {};
-  }
 
-  
+  }
+  //Salvar usuario no banco de dados e redirecionar para a ListagemUserComponent (Listagem de usuarios)
+  // e limpar o formulario de cadastro de usuario (this.model = {})
+  // addUsuario() {
+  //   if (this.model.id == 0) {
+  //     // Se o id do usuário for igual a zero então salva no banco de dados
+  //     this.userService.addUsuario(this.model).subscribe(
+  //       //(data) => {
+  //         //this.model = data;
+  //         // limpando o formulario de cadastro de usuario
+  //         //this.model = { id: 0, nome: '', idade: 0, perfil: '' };
+  //         //console.log(data);
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  //   } else {
+  //     // Se o usuário já existir no banco de dados então atualiza o usuário
+  //     this.userService.updateUsuario(this.model).subscribe((data) => {
+  //       this.model = data;
+  //       console.log(data);
+  //     });
+  //   }
+  //   console.log(this.model);
+  //   //this.model = {};
+  // }
+
+
+
 }
