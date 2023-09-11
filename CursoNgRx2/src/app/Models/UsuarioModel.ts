0287e3fc75eb2b207211f11ad4e37e4007051615
Anderson Martins <BlackCode77.1980@gmail.com>
2023-09-11 08:10:51 -0300

    Reforço de estudos do Ngrx Reducer

---
+++ CursoNgRx2/src/app/Models/UsuarioModel.ts
@@ -0,0 +1,13 @@
+export interface UsuarioModel {
+  id: number;
+  nome: string;
+  idade: number;
+  perfil: string;
+}
+
+/*
+"id": 1,
+      "nome": "Usuario_1",
+      "idade": 45,
+      "perfil": "administrador"
+*/
---
+++ CursoNgRx2/src/app/Repository/usuario.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../Models/UsuarioModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  constructor(private http: HttpClient) {}
+
+  getUsuarios() {
+    // pega todos os usuarios
+    return this.http.get<UsuarioModel[]>('http://localhost:3000/usuarios');
+  }
+
+  getUsuario(id: number) {
+    // pega usuario por id
+    return this.http.get<UsuarioModel[]>(
+      'http://localhost:3000/usuarios/' + id
+    );
+  }
+
+  addUsuario(usuario: UsuarioModel) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    return this.http.post<UsuarioModel>(
+      'http://localhost:3000/usuarios',
+      JSON.stringify(usuario),
+      { headers: headers }
+    );
+  }
+
+  updateUsuario(usuario: UsuarioModel) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    return this.http.put<UsuarioModel>(
+      'http://localhost:3000/usuarios' + usuario.id,
+      JSON.stringify(usuario),
+      { headers: headers }
+    );
+  }
+
+  deleteUsuario(id: number) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    return this.http.delete('http://localhost:3000/usuarios' + id, {
+      headers: headers,
+    });
+  }
+}
+
+
