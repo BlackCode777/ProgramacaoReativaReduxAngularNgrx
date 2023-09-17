162dfb31a91d0bba494de000888a2b795b31bbbc
Anderson Martins <BlackCode77.1980@gmail.com>
2023-09-17 00:38:57 -0300

    Minha hierarquia de arquivos NgRx redicer

--- CursoNgRx/src/app/Store/usuarios/usuarios.reducer.ts
+++ CursoNgRx/src/app/Store/usuarios/usuarios.reducer.ts
@@ -2,7 +2,7 @@ import { Action, createFeatureSelector, createReducer, createSelector, on } from
 import { UsuarioModel } from '../../models/usuario-model';
 import * as fromUsuariosAction from './usuarios.actions';
 
-// Para guaradar o estado dos usuarios
+// Para guardar o estado dos usuarios
 export interface UsuariosState {
   usuarios: UsuarioModel[];
   usuario: any | null;
@@ -21,44 +21,28 @@ const _usuariosReducer = createReducer(
   initialState,
 
   on(fromUsuariosAction.LoadUsuariosSuccess, (state, { payload }) => ({
-    ...state,
-    usuarios: payload,
-    error: '',
-  })),
+    ...state, usuarios: payload, error: '' })),
   on(fromUsuariosAction.LoadUsuariosFail, (state, { error }) => ({
-    ...state,
-    error: error,
-  })),
+    ...state, error: error })),
 
   on(fromUsuariosAction.LoadUsuarioSuccess, (state, { payload }) => ({
-    ...state,
-    usuario: payload,
-    error: '',
-  })),
+    ...state, usuario: payload, error: ''})),
   on(fromUsuariosAction.LoadUsuarioFail, (state, { error }) => ({
-    ...state,
-    error: error,
-  })),
+    ...state, error: error })),
 
   on(fromUsuariosAction.CreateUsuarioSuccess, (state, { payload }) => ({
-    ...state,
-    usuarios: [...state.usuarios, payload],
-    error: '',
-  })),
+    ...state, usuarios: [...state.usuarios, payload], error: ''})),
   on(fromUsuariosAction.CreateUsuarioFail, (state, { error }) => ({
-    ...state,
-    error: error,
-  })),
+    ...state, error: error, })),
 
   on(fromUsuariosAction.UpdateUsuariosSuccess, (state, { payload }) => ({
-    ...state,
-    usuarios: [...state.usuarios, payload].map((row) => {
-      if (row.id === payload.id) {
-        return payload;
-      } else {
-        return row;
-      }
-    }),
+    ...state, usuarios: [ ...state.usuarios, payload ].map((row) => {
+                          if (row.id === payload.id) {
+                            return payload;
+                          } else {
+                            return row;
+                          }
+                        }),
     error: '',
   })),
   on(fromUsuariosAction.UpdateUsuariosFail, (state, { error }) => ({
