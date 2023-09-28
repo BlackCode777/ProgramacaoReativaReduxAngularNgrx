3245a1c7e15de9339f38ff27a00ca29fe71a4002
Anderson Martins <BlackCode77.1980@gmail.com>
2023-09-28 06:48:50 -0300

    [TestesComJasmine] - Criando casos de teste no componente Banking

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -8,9 +8,8 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
-    })
-    .compileComponents();
+      declarations: [BankingComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BankingComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,30 @@ describe('BankingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Testando a função getPoupanca()
+  it('Teste da função getPoupanca()', () => {
+    expect(component.getPoupanca).toEqual(1000);
+  });
+
+  // Testando a função getCarteira()
+  it('Teste da função getCarteira()', () => {
+    expect(component.getCarteira).toEqual(3000);
+  });
+
+  // Testando a função setSacar()
+  it('Teste da função setSacar()', () => {
+    component.setSacar('500');
+    fixture.detectChanges(); //detectedChanges() é um método que força a atualização do componente
+    expect(component.getCarteira).toEqual(3500);
+    expect(component.getPoupanca).toEqual(500);
+  });
+
+  // Testando a função setDepositar()
+  it('Teste da função setDepositar()', () => {
+    component.setDepositar('500');
+    fixture.detectChanges(); //detectedChanges() é um método que força a atualização do componente
+    expect(component.getPoupanca).toEqual(1500);
   });
 });
