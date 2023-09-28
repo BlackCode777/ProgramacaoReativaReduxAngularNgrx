93d3e7afdce7924005926d43cf6e8a8b97a81565
Anderson Martins <BlackCode77.1980@gmail.com>
2023-09-27 22:39:52 -0300

    Configuração de rota para componente banking

--- teste-unitario/src/app/app-routing.module.ts
+++ teste-unitario/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Criand a rota do componente banking
+  {
+    path: 'banking',
+    loadChildren: () =>
+      import('../app/shared/shared/shared.module').then((m) => m.SharedModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
